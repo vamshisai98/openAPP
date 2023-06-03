2de7feac27bd4f958d9f9173ae3b7d732392e733
vamshisai98 <saivamshianirudh136@gmail.com>
2023-06-03 11:28:33 +0530

    Shuffled cards logic implemented

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,25 +7,74 @@ import { CardService } from './card.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
-  cards: string[];
-  showCards: Boolean = false
-  open: Boolean = false
-
+  cards: any[];
+  flipCardStates: boolean[];
 
   constructor(private cardService: CardService) {
     this.cards = [];
-   }
+    this.flipCardStates = [];
+  }
 
   ngOnInit(): void {
     this.cards = this.cardService.getCardData();
+
+    const flipCardStates = [];
+    for (let i = 0; i < this.cards.length; i++) {
+      flipCardStates.push(false);
+    }
   }
 
-  playGame(){
-    this.showCards = !this.showCards
-  }
-  openCard(){
-    this.showCards = !this.showCards
+  shuffleCards() {
+    this.cards = this.shuffleArray(this.cards);
+    this.flipCardStates = Array(this.cards.length).fill(true);
+
+    const cardContainer = document.querySelector('.card-container');
+    if (cardContainer) {
+      cardContainer.classList.add('shuffle-animation');
+      setTimeout(() => {
+        cardContainer.classList.remove('shuffle-animation');
+      }, 1000);
+    }
   }
 
+
+
+  playGame() {
+    this.shuffleCards();
+    const cardElements = document.querySelectorAll('.card');
+    cardElements.forEach(cardElement => {
+      cardElement.classList.remove('disabled');
+    });
+  }
+
+  openCard(index: number) {
+
+    const cardElements = document.querySelectorAll('.card');
+    cardElements.forEach(cardElement => {
+      cardElement.classList.add('disabled');
+    });
+
+    this.flipCardStates[index] = !this.flipCardStates[index];
+
+    if (this.flipCardStates[index] && index === 3) {
+      console.log("Hit");
+    } else {
+      console.log("Missed");
+    }
+
+    setTimeout(() => {
+      this.flipCardStates = Array(this.cards.length).fill(false);
+    }, 5000);
+  }
+
+  shuffleArray(array: any[]) {
+    const shuffledArray = [...array];
+
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+
+    return shuffledArray;
+  }
 }
--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,7 +1,17 @@
-export const CARD_DATA: string[] = [
-    'assets/images/Ace_of_clubs.svg',
-    'assets/images/Ace_of_hearts.svg',
-    'assets/images/Ace_of_diamonds.svg',
-    'assets/images/Ace_of_spades.svg',
-  ];
-  
+export const CARD_DATA: any[] = [{
+  id: 0,
+  image: 'assets/images/Ace_of_clubs.svg',
+},
+{
+  id: 1,
+  image: 'assets/images/Ace_of_hearts.svg',
+},
+{
+  id: 2,
+  image: 'assets/images/Ace_of_diamonds.svg',
+},
+{
+  id: 3,
+  image: 'assets/images/Ace_of_spades.svg',
+}
+];
